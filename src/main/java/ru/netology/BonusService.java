842381f9c348236e0a10c562a16a249ef43dd296
Nikita U <nikitajc@mail.ru>
2022-12-03 12:44:54 +0300

    fixed

--- src/main/java/ru/netology/BonusService.java
+++ src/main/java/ru/netology/BonusService.java
@@ -5,6 +5,9 @@ public class BonusService {
         int percent = registered ? 3 : 1;
         long bonus = amount * percent / 100;
         long limit = 500;
+        if (amount < 0) {
+            bonus = 0;
+        }
         if (bonus > limit) {
             bonus = limit;
         }
